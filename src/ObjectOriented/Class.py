50c84cd2659e630fa410f4f7fee5f603bf1de0bc
Valentine23v <1175566927@qq.com>
2018-03-02 14:10:58 +0800

    ClassMethod

--- src/ObjectOriented/Class.py
+++ src/ObjectOriented/Class.py
@@ -1,6 +1,6 @@
-
+#类
 class Student(object):
-
+#class后面紧接着是类名，即Student，类名通常是大写开头的单词，紧接着是(object)，表示该类是从哪个类继承下来的，继承的概念我们后面再讲，通常，如果没有合适的继承类，就使用object类，这是所有类最终都会继承的类。
     def __init__(self,name,score):
         self.name = name
         self.score = score
@@ -8,9 +8,23 @@ class Student(object):
     def print_score(self):
         print('%s: %s' % (self.name, self.score))
 
+    def getgrade(self):
+        if self.score>=90:
+            print('A')
+        elif self.score>=80:
+            print('B')
+        elif self.score>=60:
+            print('C')
+        else:
+            print('D')
+
+
+#实例化一个类用类名+()
+#由于类可以起到模板的作用，因此，可以在创建实例的时候，把一些我们认为必须绑定的属性强制填写进去。通过定义一个特殊的__init__方法，在创建实例的时候，就把name，score等属性绑上去
 
 bart = Student('Bart Simpson', 59)
 lisa = Student('Lisa Simpson', 87)
 bart.print_score()
 lisa.print_score()
 
+bart.getgrade()
