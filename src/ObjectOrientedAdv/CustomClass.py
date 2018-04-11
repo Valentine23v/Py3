9af3667d78e5db013effd6190115e43cd2fb0cd1
Valentine23v <1175566927@qq.com>
2018-04-11 17:41:42 +0800

    Commit

--- src/ObjectOrientedAdv/CustomClass.py
+++ src/ObjectOrientedAdv/CustomClass.py
@@ -4,8 +4,111 @@
 # __slots__我们已经知道怎么用了，__len__()方法我们也知道是为了能让class作用于len()函数。
 # 除此之外，Python的class中还有许多这样有特殊用途的函数，可以帮助我们定制类。
 
+# __str__
+
+# class Student(object):
+#     def __init__(self, name):
+#         self.name = name
+# print(Student('Michael'))
+# <__main__.Student object at 0x109afb190>
+# 怎么才能打印得好看呢？只需要定义好__str__()方法，返回一个好看的字符串就可以了：
+
 class Student(object):
     def __init__(self, name):
         self.name = name
+    def __str__(self):
+        return 'Student Object (name: %s)' % self.name
+    __repr__ = __str__
+
 print(Student('Michael'))
-#<__main__.Student object at 0x109afb190>
+s = Student('Michael')
+#直接敲变量不用print，打印出来的实例还是不好看
+#直接显示变量调用的不是__str__()，而是__repr__()，两者的区别是__str__()返回用户看到的字符串，而__repr__()返回程序开发者看到的字符串，也就是说，__repr__()是为调试服务的。
+#解决办法是再定义一个__repr__()。但是通常__str__()和__repr__()代码都是一样的
+
+# __iter__
+# 如果一个类想被用于for ... in循环，类似list或tuple那样，就必须实现一个__iter__()方法，
+# 该方法返回一个迭代对象，然后，Python的for循环就会不断调用该迭代对象的__next__()方法拿到循环的下一个值，直到遇到StopIteration错误时退出循环。
+
+class Fib(object):
+    def __init__(self):
+        self.a, self.b = 0, 1 # 初始化两个计数器a，b
+
+    def __iter__(self):
+        return self # 实例本身就是迭代对象，故返回自己
+
+    def __next__(self):
+        self.a, self.b = self.b, self.a + self.b # 计算下一个值
+        if self.a > 100000: # 退出循环的条件
+            raise StopIteration()
+        return self.a # 返回下一个值
+
+for n in Fib():
+    print(n)
+
+# __getitem__
+
+# Fib实例虽然能作用于for循环，看起来和list有点像，但是，把它当成list来使用还是不行
+# 要表现得像list那样按照下标取出元素，需要实现__getitem__()方法：
+
+class Fib2(object):
+    def __getitem__(self, n):
+        a, b = 1, 1
+        for x in range(n):
+            a, b = b, a + b
+        return a
+f = Fib2()
+print(f[5])
+print(f[10])
+
+# list有个神奇的切片方法
+# 对于Fib却报错。原因是__getitem__()传入的参数可能是一个int，
+# 也可能是一个切片对象slice，所以要做判断
+
+class Fib3(object):
+    def __getitem__(self, n):
+        if isinstance(n, int): # n是索引
+            a, b = 1, 1
+            for x in range(n):
+                a, b = b, a + b
+            return a
+        if isinstance(n, slice): # n是切片
+            start = n.start
+            stop = n.stop
+            if start is None:
+                start = 0
+            a, b = 1, 1
+            L = []
+            for x in range(stop):
+                if x >= start:
+                    L.append(a)
+                a, b = b, a + b
+            return L
+f3 = Fib3()
+
+print(f3[0:5])
+# 但是没有对step参数作处理
+# 也没有对负数作处理，所以，要正确实现一个__getitem__()还是有很多工作要做的。
+# 此外，如果把对象看成dict，__getitem__()的参数也可能是一个可以作key的object，例如str。
+# 与之对应的是__setitem__()方法，把对象视作list或dict来对集合赋值。最后，还有一个__delitem__()方法，用于删除某个元素。
+# 总之，通过上面的方法，我们自己定义的类表现得和Python自带的list、tuple、dict没什么区别，这完全归功于动态语言的“鸭子类型”，不需要强制继承某个接口。
+
+# __getattr__
+
+# 正常情况下，当我们调用类的方法或属性时，如果不存在，就会报错。
+# 要避免这个错误，除了可以加上一个score属性外，Python还有另一个机制，那就是写一个__getattr__()方法，动态返回一个属性。
+
+class Student(object):
+
+    def __init__(self):
+        self.name = 'Michael'
+
+    def __getattr__(self, attr):
+        if attr=='score':
+            return 99
+# 返回函数也是完全可以的
+    def __getattr__(self, attr):
+        if attr=='age':
+            return lambda :25
+
+
