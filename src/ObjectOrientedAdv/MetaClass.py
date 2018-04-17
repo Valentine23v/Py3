da46cbe71c75740ff4cea6f78b0128c63fb9f207
Valentine23v <1175566927@qq.com>
2018-04-17 18:08:20 +0800

    Commit

---
+++ src/ObjectOrientedAdv/MetaClass.py
@@ -0,0 +1,23 @@
+# 元类
+
+class Hello(object):
+    def hello(self, name='world'):
+        print('Hello, %s.' % name)
+
+h = Hello()
+h.hello()
+print(type(Hello))
+print(type(h))
+# Hello是一个class，它的类型就是type，而h是一个实例，它的类型就是class Hello
+
+# 我们说class的定义是运行时动态创建的，而创建class的方法就是使用type()函数。
+# type()函数既可以返回一个对象的类型，又可以创建出新的类型，比如，我们可以通过type()函数创建出Hello类，而无需通过class Hello(object)...的定义
+
+def fn(self,name='world'): # 先定义函数
+    print('Hello, %s' %name)
+
+HelloA = type('Hello', (object), dict(hello=fn)) # 创建Hello class\
+
+a = HelloA()
+a.hello()
+
